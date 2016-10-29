bd34854b9adbe320cbf115ca79645f47081554c3
Sebastian kanis <code@sebastiankanis.de>
2016-10-29 08:20:12 +0200

    removing invalid char

--- pi-blaster-mock.py
+++ pi-blaster-mock.py
@@ -1,4 +1,4 @@
-1#!/usr/bin/python3
+#!/usr/bin/python3
 # Copyright (c) 2016 Sebastian Kanis
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
